59e9a0586bbb235a3ad95c0bee0e58aae354289a
ameer suhail <kameer@gmail.com>
2022-11-11 00:53:14 +0530

    added event listeners in index.js to respond to button clicks and keyboard inputs

---
+++ index.js
@@ -0,0 +1,47 @@
+var numofbuttons = document.querySelectorAll(".drum").length;
+// to send the key by mouseclick on button
+for (var i = 0; i < numofbuttons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonClicked = this.innerHTML;
+
+        makeSound(buttonClicked);
+    });
+}
+// to send the key pressed on keyboard
+document.addEventListener("keydown", function (event) {
+    // added tolowercase in order to work even if the capslock is on 
+    makeSound(event.key.toLowerCase());
+})
+
+function makeSound(buttonClicked) {
+    switch (buttonClicked) {
+        case 'w':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+    }
+}
