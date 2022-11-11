c854d3fb0510e83eaada36730bbff526b1a0f0b7
ameer suhail <kameer@gmail.com>
2022-11-11 09:10:53 +0530

    added button animation using timeout function

--- index.js
+++ index.js
@@ -50,5 +50,13 @@ function makeSound(buttonClicked) {
     }
 }
 
-
+function buttonAnimation(pressedKey){
+    var activeButton = document.querySelector(`.${pressedKey}`);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(removeFunction, 100)
+    function removeFunction(){
+        activeButton.classList.remove("pressed");
+    }
+    }
 
